bcb97b46346cf503259ad8fed1b510a41fe4abf0
MapaMelo <148729836+MapaMelo@users.noreply.github.com>
2025-11-11 18:21:13 +0000

    Agregar fotos

--- script.js
+++ script.js
@@ -3,16 +3,40 @@ const listaCabezas = [
   "../cambio_personajes/assets/img/cabeza1.jpg",
   "../cambio_personajes/assets/img/cabeza2.jpg",
   "../cambio_personajes/assets/img/cabeza3.png",
+  "../cambio_personajes/assets/img/cabeza 4.png",
+  "../cambio_personajes/assets/img/cabeza 5.png",
+  "../cambio_personajes/assets/img/cabeza 6.png",
+  "../cambio_personajes/assets/img/cabeza 7.png",
+  "../cambio_personajes/assets/img/cabeza 8.png",
+  "../cambio_personajes/assets/img/cabeza 9.png",
+  "../cambio_personajes/assets/img/cabeza 10.png",
+  "../cambio_personajes/assets/img/cabeza 11.png",
 ];
 const listaTroncos = [
  "../cambio_personajes/assets/img/tronco1.jpg",
   "../cambio_personajes/assets/img/tronco2.jpg",
   "../cambio_personajes/assets/img/tronco3.jpg",
+  "../cambio_personajes/assets/img/cuerpo4.jpg",
+  "../cambio_personajes/assets/img/cuerpo5.jpg",
+  "../cambio_personajes/assets/img/cuerpo6.jpg",
+  "../cambio_personajes/assets/img/cuerpo7.jpg",
+  "../cambio_personajes/assets/img/cuerpo8.jpg",
+  "../cambio_personajes/assets/img/cuerpo9.jpg",
+  "../cambio_personajes/assets/img/cuerpo10.jpg",
+  "../cambio_personajes/assets/img/cuerpo11.jpg",
 ];
 const listaPatas = [
   "../cambio_personajes/assets/img/patas1.jpg",
   "../cambio_personajes/assets/img/patas2.jpg",
   "../cambio_personajes/assets/img/patas3.png",
+  "../cambio_personajes/assets/img/patas4.png",
+  "../cambio_personajes/assets/img/patas5.png",
+  "../cambio_personajes/assets/img/patas6.png",
+  "../cambio_personajes/assets/img/patas7.png",
+  "../cambio_personajes/assets/img/patas8.png",
+  "../cambio_personajes/assets/img/patas9.png",
+  "../cambio_personajes/assets/img/patas10.png",
+  "../cambio_personajes/assets/img/patas11png",
 ];
 
 // Obtenemos los contenedores de las imágenes del HTML usando los IDs
