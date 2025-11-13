80962afeaaeaa95c2197155caef600bd153ec925
MapaMelo <148729836+MapaMelo@users.noreply.github.com>
2025-11-13 11:46:25 +0000

    codigo y fotos

--- script.js
+++ script.js
@@ -30,16 +30,29 @@ const listaPatas = [
   "../cambio_personajes/assets/img/patas11.jpg",
 ];
 
+const listaMascotas = [
+
+  "../cambio_personajes/assets/img/mascota1.jpg",
+  "../cambio_personajes/assets/img/mascota2.jpg",
+  "../cambio_personajes/assets/img/mascota3.jpg",
+  "../cambio_personajes/assets/img/mascota4.jpg",
+  "../cambio_personajes/assets/img/mascota5.jpg",
+  "../cambio_personajes/assets/img/mascota6.jpg",
+  "../cambio_personajes/assets/img/mascota7.jpg",
+];
+
 // Obtenemos los contenedores de las imágenes del HTML usando los IDs
 const cabeza = document.getElementById("cabeza");
 const tronco = document.getElementById("tronco");
 const patas = document.getElementById("patas");
 const boton = document.getElementById("boton");
+const mascota = document.getElementById("mascota"); 
 
 // Inicializamos las variables de los números aleatorios
 let cabezaAleatorio = 0;
 let troncoAleatorio = 0;
 let patasAleatorio = 0;
+let mascotaAleatorio = 0;
 
 // Función para generar un número aleatorio entre dos valores
 function numeroAleatorio(min, max) {
@@ -51,13 +64,15 @@ function generarBestia() {
   cabezaAleatorio = numeroAleatorio(0, listaCabezas.length);
   troncoAleatorio = numeroAleatorio(0, listaTroncos.length);
   patasAleatorio = numeroAleatorio(0, listaPatas.length);
+  mascotaAleatorio = numeroAleatorio(0, listaMascotas.length);
 
   // Asignamos la nueva fuente (source) a cada imagen
   cabeza.src = `${listaCabezas[cabezaAleatorio]}`; //ruta + listaCabezas[cabezaAleatorio];
   tronco.src = `${listaTroncos[troncoAleatorio]}`;
   patas.src = `${listaPatas[patasAleatorio]}`;
+  mascota.src = `${listaMascotas[mascotaAleatorio]}`;
 
-  console.log(cabezaAleatorio, troncoAleatorio, patasAleatorio);
+  console.log(cabezaAleatorio, troncoAleatorio, patasAleatorio, mascotaAleatorio);
 }
 
 // Generamos un nuevo collage cada vez que hacemos click en el botón "mezclar"
